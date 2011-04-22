517a06aa3f2edf36114a8e3b5202ed21be4cc4e7
Micah Anderson <micah@riseup.net>
2011-04-22 16:45:50 -0400

    fix problem where you are not on wheezy or sid, but you install a newer version through puppetmaster_ensure_version (such as through backports)

--- manifests/puppetmaster/package/debian.pp
+++ manifests/puppetmaster/package/debian.pp
@@ -1,13 +1,21 @@
 class puppet::puppetmaster::package::debian inherits puppet::puppetmaster::package::base {
 
-  if !$puppetmaster_ensure_version { $puppetmaster_ensure_version = 'installed' }
-  case $lsbdistcodename {
+  if !$puppetmaster_ensure_version {
+    $puppetmaster_ensure_version = 'installed'
+    case $lsbdistcodename {
       wheezy,sid: {
         $puppetmaster_common_required = 'puppetmaster-common'
         $puppetmaster_common_ensure = $puppetmaster_ensure_version
       }
+      default: {
+        if (versioncmp($puppetmaster_ensure_version, "2.6.7") >= 0) {
+          $puppetmaster_common_requred = 'puppetmaster-common'
+          $puppetmaster_common_ensure = $puppetmaster_ensure_version
+        }
+      }
+    }
   }
-
+  
   Package["puppetmaster"]{
         require => $puppetmaster_common_required ? {
         '' => undef,
