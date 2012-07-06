931f021a1fe45636aec3b5332848dd912425affe
varac <varacanero@zeromail.org>
2012-07-06 16:20:50 +0000

    notify_passenger_puppetmaster: do apache2 reload
    instead of touching .../restart.txt, which doen't seem to
    work with libapache2-mod-passenger 2.2.11debian-2

--- manifests/puppetmaster/linux.pp
+++ manifests/puppetmaster/linux.pp
@@ -3,7 +3,8 @@ class puppet::puppetmaster::linux inherits puppet::linux {
   if $puppetmaster_mode == 'passenger' {
     exec { 'notify_passenger_puppetmaster':
       refreshonly => true,
-      command => 'touch /etc/puppet/rack/tmp/restart.txt && sleep 1 && rm /etc/puppet/rack/tmp/restart.txt',
+      #command => 'touch /etc/puppet/rack/tmp/restart.txt && sleep 1 && rm /etc/puppet/rack/tmp/restart.txt',
+      command => '/etc/init.d/apache2 reload',
     }
   } else {
     service { 'puppetmaster':
