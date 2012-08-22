0097eeef0b24a85d6f067bbf5b126d3fa22f1c15
Micah Anderson <micah@riseup.net>
2012-08-22 14:40:59 -0400

    implement fixes from #3514

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,21 +1,19 @@
 class puppet::debian inherits puppet::linux {
 
   file { '/etc/default/puppet':
-    source => [ "puppet:///modules/site-puppet/client/debian/${fqdn}/puppet",
-                "puppet:///modules/site-puppet/client/debian/${domain}/puppet",
-                "puppet:///modules/site-puppet/client/debian/puppet",
+    source => [ "puppet:///modules/site_puppet/client/debian/${fqdn}/puppet",
+                "puppet:///modules/site_puppet/client/debian/${domain}/puppet",
+                "puppet:///modules/site_puppet/client/debian/puppet",
                 "puppet:///modules/puppet/client/debian/puppet" ],
     notify => Service[puppet],
     owner => root, group => 0, mode => 0644;
   }  
 
-  case $lsbdistcodename {
-    squeeze,sid: {
-      $real_puppet_hasstatus = true
-    }
-    default: {
-      $real_puppet_hasstatus = false
-    }
+  if versioncmp($puppetversion,'2.6') >= 0 {
+    $real_puppet_hasstatus = true
+  }
+  else {
+    $real_puppet_hasstatus = false
   }
     
   Service[puppet]{
--- manifests/puppetmaster/debian.pp
+++ manifests/puppetmaster/debian.pp
@@ -1,21 +1,26 @@
-class puppet::puppetmaster::debian inherits puppet::puppetmaster::package {
+class puppet::puppetmaster::debian {
 
+  include puppet::puppetmaster::package
+  include puppet::puppetmaster::linux
+  
   if $puppetmaster_mode != 'passenger' {
-    case $lsbdistcodename {
-      squeeze,sid: {
-        Service['puppetmaster'] { hasstatus => true, hasrestart => true }
-      }
+    if $puppet_majorversion >= '2.6' {  
+      Service['puppetmaster'] { hasstatus => true, hasrestart => true }
     }
   }
 
+  if $puppetmaster_mode == 'passenger' {
+    $puppetmaster_default_nofity = 'Exec[notify_passenger_puppetmaster]'
+  }
+  
   file { '/etc/default/puppetmaster':
-    source => [ "puppet:///modules/site-puppet/master/debian/${fqdn}/puppetmaster",
-                "puppet:///modules/site-puppet/master/debian/${domain}/puppetmaster",
-                "puppet:///modules/site-puppet/master/debian/puppetmaster",
+    source => [ "puppet:///modules/site_puppet/master/debian/${fqdn}/puppetmaster",
+                "puppet:///modules/site_puppet/master/debian/${domain}/puppetmaster",
+                "puppet:///modules/site_puppet/master/debian/puppetmaster",
                 "puppet:///modules/puppet/master/debian/puppetmaster" ],
-    notify => $puppetmaster_mode ? {
-      'passenger' => Exec['notify_passenger_puppetmaster'],
-      default => Service[puppetmaster],
+    notify => $puppetmaster_default_nofity ? {
+    '' => Service[puppetmaster],
+    default => Exec['notify_passenger_puppetmaster']
     },
     owner => root, group => 0, mode => 0644;
   }
