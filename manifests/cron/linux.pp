2a98b04c08087fc27fea067861c3e4fa08af36a7
varac <varacanero@zeromail.org>
2013-01-02 14:21:59 +0100

    linted cron/linux.pp

--- manifests/cron/linux.pp
+++ manifests/cron/linux.pp
@@ -1,11 +1,12 @@
-# manifests/cron/linux.pp 
 class puppet::cron::linux inherits puppet::linux {
 
   include puppet::cron::base
   if !$puppet_config { $puppet_config = '/etc/puppet/puppet.conf' }
-  if $puppet_http_compression { $puppet_http_compression_str = '--http_compression' }
+  if $::puppet_http_compression {
+    $puppet_http_compression_str = '--http_compression'
+  }
 
-  
+
   if !$puppet_crontime {
     $puppet_crontime_interval_minute = fqdn_rand(29)
     $puppet_crontime_interval_minute2 = inline_template('<%= 30+puppet_crontime_interval_minute.to_i %>')
@@ -13,9 +14,9 @@ class puppet::cron::linux inherits puppet::linux {
   }
 
   File['/etc/cron.d/puppetd.cron']{
-    source => undef,
-    content => "#run puppet\n$puppet_crontime root output=\$(/usr/sbin/puppetd --onetime --no-daemonize --splay --config=/etc/puppet/puppet.conf --color false); ret=\$?; printf \"\\%s\" \"\$output\" | grep -E '(^err:|^alert:|^emerg:|^crit:)'; exit \$ret\n",
-    before => Service['puppet'],
-    ensure => present
+    source  => undef,
+    content => "#run puppet\n${puppet_crontime} root output=\$(/usr/sbin/puppetd --onetime --no-daemonize --splay --config=/etc/puppet/puppet.conf --color false); ret=\$?; printf \"\\%s\" \"\$output\" | grep -E '(^err:|^alert:|^emerg:|^crit:)'; exit \$ret\n",
+    before  => Service['puppet'],
+    ensure  => present
   }
 }
