0cb07edbbb3952a2c787a15642c93e41bec3c330
intrigeri <intrigeri@boum.org>
2013-04-23 17:54:09 +0200

    Install puppetlast cronjob under a usable name.
    That is, install it as /etc/cron.d/puppetlast,
    and cleanup the old buggy /etc/cron.d/puppetlast.cron.
    
    As cron(8) says about /etc/cron.d/: "For example, any file containing dots will
    be ignored."

--- manifests/puppetmaster/checklastrun.pp
+++ manifests/puppetmaster/checklastrun.pp
@@ -21,12 +21,16 @@ class puppet::puppetmaster::checklastrun {
       group  => 0,
       mode   => '0700';
 
-    '/etc/cron.d/puppetlast.cron':
+    '/etc/cron.d/puppetlast':
       content => "${puppetmaster_lastruncheck_cron} root /usr/local/sbin/puppetlast ${puppet_lastruncheck_timeout_str} ${puppet_lastruncheck_ignorehosts_str} ${$puppet_lastruncheck_additionaloptions}\n",
       require => File['/usr/local/sbin/puppetlast'],
       owner   => root,
       group   => 0,
       mode    => '0644',
-      notify  => Service['cron']
+      notify  => Service['cron'];
+
+    # Cleanup cronjob previously installed under a buggy name.
+    '/etc/cron.d/puppetlast.cron':
+      ensure => absent;
   }
 }
