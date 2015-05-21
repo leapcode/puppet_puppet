55677983a65e03efda27b5ec9419c77ce9b8f72a
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2015-05-21 14:53:06 -0400

    Fix storeconfig dependencies on Debian

--- manifests/master/storeconfigs.pp
+++ manifests/master/storeconfigs.pp
@@ -2,6 +2,13 @@
 # so storeconfigs works.
 
 class puppet::master::storeconfigs {
-  include rails
+  if $::operatingsystem == 'Debian' {
+    ensure_packages(['ruby-activerecord'])
+    if $::operatingsystemmajrelease == 8 {
+      ensure_packages(['ruby-activerecord-deprecated-finders'])
+    }
+  } else {
+    include rails
+  }
   include mysql::client::ruby
 }
